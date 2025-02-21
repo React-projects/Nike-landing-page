5d227ea31b647eff46c9dbd055c0f87ca6b88e03
BAssal <baslm8496@gmail.com>
2025-02-22 00:36:30 +0200

    Add main page sections and navigation to App component

---
+++ src/Components/Nav.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div>
+testy
+    </div>
+  )
+}
+
+export default Nav
---
+++ src/Sections/Imports.js
@@ -0,0 +1,10 @@
+import Hero from './Hero'
+import Services from './Services'
+import PopularProducts from './PopularProducts'
+import SuperQuality from './SuperQuality'
+import SpecialOffers from './SpecialOffers'
+import CustomerReviews from './CustomerReviews'
+import Subscribe from './Subscribe'
+import Footer from './Footer'
+
+export { Hero, Services, PopularProducts, SuperQuality, SpecialOffers, CustomerReviews, Subscribe, Footer }
