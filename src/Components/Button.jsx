4d80c7a7dc56f3d4cd0029a61c396846e8ca561c
BAssal <baslm8496@gmail.com>
2025-02-23 12:52:31 +0200

    Add Button and Nav components with responsive design

---
+++ src/Components/Button.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Button = ({label, iconURL, }) => {
+  return (
+    <button className='flex justify-center items-center gap-2 px-7 py-4 font-montserrat text-lg leading-none text-white rounded-full bg-coral-red border border-coral-red'>{label}
+    <img src={iconURL} alt="arrow right"  className='ml-2 rounded-full w-5 h-5' />
+
+    </button>
+
+  )
+}
+
+export default Button
--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,11 +1,30 @@
 import React from 'react'
+import HeaderLogo from '../assets/images/header-logo.svg'
+import hamburger from '../assets/icons/hamburger.svg'
+import { navLinks } from '../constants'
 
-const Nav = () => {
-  return (
-    <div>
-testy
-    </div>
-  )
+const Nav = () =>
+{
+    return (
+        <header className='padding-x py-8 absolute z-10 w-full'>
+            <nav className='flex justify-between items-center max-container'>
+                <a href="/" aria-label='logo'>
+                    <img src={HeaderLogo} alt="logo" width={130} height={29} />
+
+                </a>
+                <ul className='flex-1 flex justify-center items-center gap-16 max-lg:hidden  text-slate-gray'>
+                    {navLinks.map((item) => (
+                        <li key={item.label}>
+                            <a href={item.href} className='font-montserrat leading-normal'>{item.label}</a>
+                        </li>
+                    ))}
+                </ul>
+                <div className=' hidden max-lg:block'>
+                    <img src={hamburger} alt="hamburger" width={24} height={24} />
+                </div>
+            </nav>
+        </header>
+    )
 }
 
 export default Nav
