34d717954587dd8faa6f9f25814d4fcf23fc2ade
BAssal <baslm8496@gmail.com>
2025-02-23 22:25:52 +0200

    Add Services section with dynamic ServiceCard rendering

--- src/Sections/Services.jsx
+++ src/Sections/Services.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import { services } from '../constants'
+import ServiceCard from '../Components/ServiceCard'
 
 const Services = () => {
   return (
-    <div>
-
-    </div>
+  <section id='services' className='max-container flex justify-center flex-wrap gap-9'>
+    {services.map((service) => (
+        <ServiceCard key={service.label} {...service} />
+    ))}
+  </section>
   )
 }
 
