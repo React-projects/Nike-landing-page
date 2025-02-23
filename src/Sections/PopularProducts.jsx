a776f3774418bacdf5a94dd2a90ca722b4b01e3b
BAssal <baslm8496@gmail.com>
2025-02-23 21:20:38 +0200

    Add PopularProducts section with dynamic product rendering

--- src/Sections/PopularProducts.jsx
+++ src/Sections/PopularProducts.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import { products } from '../constants'
+import PopularProductCard from '../Components/PopularProductCard'
 
 const PopularProducts = () => {
   return (
-    <div>
-
+   <section id='products' className='max-container max-sm:mt-12'>
+    <div className='flex flex-col justify-start gap-5'>
+    <h2 className='text-4xl font-bold font-palanquin'>Our <span className='text-coral-red'>Popular</span> Products</h2>
+    <p className='lg:max-w-lg font-montserrat text-slate-gray mt-2'>Experience top-notch quality and style with our sought-after products</p>
+    <div className='mt-16 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 sm:gap-4 gap-14'>
+    {products.map((product) => (
+        <PopularProductCard key={product.name} {...product} />
+    ))}
     </div>
+    </div>
+</section>
   )
 }
 
